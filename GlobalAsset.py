173dd3989977125883b4e14cf12a2e6a5f4ff64f
pootisplank <ialelin3401@gmail.com>
2020-07-08 12:14:31 -0700

    Token refreshing

--- GlobalAsset.py
+++ GlobalAsset.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 from getpass import getpass
 from QualysAPI import formatResponse
 
@@ -26,7 +27,27 @@ def getToken():
     token = formatResponse(request)
     print("Token receieved, login successful.")
     print(token, file=open("token.txt", "w"))
+    print(datetime.datetime.now(), file=open("token.txt", "a"))
     return token
 
+
+# Checks if token has expired.  If so, generates a new token.
+def refreshToken():
+    # Gets when the token was generated.
+    with open('token.txt', 'r') as file:
+        file.readline()
+        date = file.readline().strip('\n')
+
+    # Computes how much time as passed since token was generated.
+    token_time = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f')
+    current_time = datetime.datetime.now()
+    time_difference = (current_time - token_time).total_seconds() / 3600
+
+    # If the token was generated four hours or later ago, get a new token.
+    if (time_difference >= 4):
+        print("Token Expired, please login again.")
+        getToken()
+
     
-getToken()
+#getToken()
+refreshToken()
